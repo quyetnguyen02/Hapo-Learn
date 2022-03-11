0580cf0b5a06f99cffbbd8ac0ed11b63b15ebea2
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-03-11 17:25:18 +0700

    fix cmt

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,6 +1,1 @@
-$(document).ready(function () {
-  $('#menu-bar').click(function () {
-    $('#menu-bar').toggleClass('fa-xmark');
-    $('#nav').toggleClass('show');
-  })
-})
+require('./header');
