58d0b8a1d0cb5a646b8b99623bfa44e6321b061a
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-03-15 13:48:17 +0700

    fix id name

--- public/app/js/header.js
+++ public/app/js/header.js
@@ -7,27 +7,27 @@ $(document).ready(function() {
   //click to login/register
   $('#login-register').click(function() {
     $('#login').addClass('active');
-    $('#close-form-login').css('display','block');
-    $('#btn-login').addClass('link-login')
-    $('#close-register-form').css('display','none');
-    $('#btn-register').removeClass('link-register')
+    $('#closeFormLogin').css('display','block');
+    $('#btnLogin').addClass('link-login')
+    $('#closeRegisterForm').css('display','none');
+    $('#btnRegister').removeClass('link-register')
   })
   //close form login/register
   $('#close').click(function() {
     $('#login').removeClass('active');
   })
 
-  $('#btn-register').click(function() {
-    $('#close-form-login').css('display','none');
-    $('#close-register-form').css('display','block');
-    $('#btn-login').removeClass('link-login')
-    $('#btn-register').addClass('link-register')
+  $('#btnRegister').click(function() {
+    $('#closeFormLogin').css('display','none');
+    $('#closeRegisterForm').css('display','block');
+    $('#btnLogin').removeClass('link-login')
+    $('#btnRegister').addClass('link-register')
   })
 
-  $('#btn-login').click(function() {
-    $('#close-form-login').css('display','block');
-    $('#close-register-form').css('display','none');
-    $('#btn-login').addClass('link-login')
-    $('#btn-register').removeClass('link-register')
+  $('#btnLogin').click(function() {
+    $('#closeFormLogin').css('display','block');
+    $('#closeRegisterForm').css('display','none');
+    $('#btnLogin').addClass('link-login')
+    $('#btnRegister').removeClass('link-register')
   })
 })
