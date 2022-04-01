34b1a103c00e14c5cbb9869bc9b9826b04860559
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-04-01 11:42:05 +0700

    fix SoftDeletes

--- database/migrations/2022_03_31_015159_create_courses_table.php
+++ database/migrations/2022_03_31_015159_create_courses_table.php
@@ -15,7 +15,7 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable()->unique();
+            $table->string('name')->nullable();
             $table->string('description')->nullable();
             $table->double('time')->nullable();
             $table->double('price')->nullable();
