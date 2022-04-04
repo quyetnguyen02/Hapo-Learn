e37d5cc7b03b4f6eefe21d56f03a0138ca92e098
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-04-04 15:21:45 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[\App\Http\Controllers\HomeController::class,'index']);
-Route::get('/courses/detail',[\App\Http\Controllers\HomeController::class,'show'])->name('home.show');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [\App\Http\Controllers\HomeController::class,'index']);
+Route::get('/courses/detail', [\App\Http\Controllers\HomeController::class,'show'])->name('home.show');
