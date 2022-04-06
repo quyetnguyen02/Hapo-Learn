23c842ee79de051ef0571ba1990f0ef8cd8c32dc
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-04-06 15:36:47 +0700

    fix convention

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -77,7 +77,7 @@
             <div class="row align-items-center flex-wrap-reverse banner-row">
                 <div class="col-md-6 banner-content">
                     <p class="banner-title">Learn&nbspAnytime,&nbspAnywhere</p>
-                    <p class="banner-logo">at HapoLearn <img class="icon-owl" src="{{asset('image/icon_owl.png')}}"
+                    <p class="banner-logo">at HapoLearn <img class="icon-owl" src="{{ asset('image/icon_owl.png') }}"
                                                              alt="icon-owl">!
                     </p>
                     <p class="banner-text">Interactive lessons, "on-the-go" <br>
@@ -96,11 +96,11 @@
                 @foreach($courses as $course)
                     <div class="card">
                         <div class="card-img img-courses html-tut">
-                            <img src="{{$course->image}}" alt="Html Logo">
+                            <img src="{{ $course->image }}" alt="Html Logo">
                         </div>
                         <div class="card-body">
-                            <p class="card-title">{{$course->name}}</p>
-                            <p class="card-text">{{$course->description}}</p>
+                            <p class="card-title">{{ $course->name }}</p>
+                            <p class="card-text">{{ $course->description }}</p>
                             <a href="{{route('home.show')}}" class="btn btn-courses">Take This Course</a>
                         </div>
                     </div>
@@ -119,12 +119,12 @@
                 @foreach($courses as $course)
                     <div class="card">
                         <div class="card-img img-courses html-tut">
-                            <img src="{{$course->image}}" alt="Html Logo">
+                            <img src="{{ $course->image }}" alt="Html Logo">
                         </div>
                         <div class="card-body">
-                            <p class="card-title">{{$course->name}}</p>
-                            <p class="card-text">{{$course->description}}</p>
-                            <a href="{{route('home.show')}}" class="btn btn-courses">Take This Course</a>
+                            <p class="card-title">{{ $course->name }}</p>
+                            <p class="card-text">{{ $course->description }}</p>
+                            <a href="#" class="btn btn-courses">Take This Course</a>
                         </div>
                     </div>
                 @endforeach
@@ -138,30 +138,30 @@
     <section class="why-hp">
         <div class="container-fluid why-container">
             <div class="mb-left">
-                <img src="{{asset('image/mb_left).png')}}" alt="mb left">
+                <img src="{{ asset('image/mb_left).png') }}" alt="mb left">
             </div>
             <b class="content-why-hp">Why HapoLearn?</b>
             <div class="why-down d-flex text-center justify-content-between">
                 <div class="content d-flex justify-content-end align-items-center">
                     <b>Why HapoLearn?</b>
-                    <p class="why-content"><img src="{{asset('image/icon.png')}}" alt="icon tick">Interactive lessons,
+                    <p class="why-content"><img src="{{ asset('image/icon.png') }}" alt="icon tick">Interactive lessons,
                         "on-the-go" practice,
                         peer support.</p>
-                    <p class="why-content"><img src="{{asset('image/icon.png')}}" alt="icon tick">Interactive lessons,
+                    <p class="why-content"><img src="{{ asset('image/icon.png') }}" alt="icon tick">Interactive lessons,
                         "on-the-go" practice,
                         peer support.</p>
-                    <p class="why-content"><img src="{{asset('image/icon.png')}}" alt="icon tick">Interactive lessons,
+                    <p class="why-content"><img src="{{ asset('image/icon.png') }}" alt="icon tick">Interactive lessons,
                         "on-the-go" practice,
                         peer support.</p>
-                    <p class="why-content"><img src="{{asset('image/icon.png')}}" alt="icon tick">Interactive lessons,
+                    <p class="why-content"><img src="{{ ('image/icon.png') }}" alt="icon tick">Interactive lessons,
                         "on-the-go" practice,
                         peer support.</p>
-                    <p class="why-content"><img src="{{asset('image/icon.png')}}" alt="icon tick">Interactive lessons,
+                    <p class="why-content"><img src="{{ asset('image/icon.png') }}" alt="icon tick">Interactive lessons,
                         "on-the-go" practice,
                         peer support.</p>
                 </div>
                 <div class="mb-right d-flex align-items-center">
-                    <img src="{{asset('image/mb_right.png')}}" alt="mb-right">
+                    <img src="{{ asset('image/mb_right.png') }}" alt="mb-right">
                 </div>
             </div>
         </div>
@@ -184,17 +184,17 @@
                             <div class="feed-back-up">
                                 <p class="feed-back-border"></p>
                                 <p class="feed-back-up-content">
-                                    {{$review->content}}
+                                    {{ $review->content }}
                                 </p>
                                 <i class="fa-solid fa-sort-down"></i>
                             </div>
                             <div class="feed-back-down">
                                 <div class="feed-back-img">
-                                    <img src="{{$review->user->avatar}}" alt="Hapo Avatar">
+                                    <img src="{{ $review->user->avatar }}" alt="Hapo Avatar">
                                 </div>
                                 <div class="feed-back-down-content">
-                                    <p class="feed-back-name">{{$review->user->name}}</p>
-                                    <p class="feed-back-span">{{$review->user->job}}</p>
+                                    <p class="feed-back-name">{{ $review->user->name }}</p>
+                                    <p class="feed-back-span">{{ $review->user->job }}</p>
                                     <p class="feed-back-star">
                                         @switch($review->vote)
                                             @case(1)
